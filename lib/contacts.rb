c496d7b7d68944c9f2936d726162707512635504
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-27 20:55:30 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,8 +18,7 @@ def remove_strawberry(contacts)
   contact.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-
       end
-    binding.pry
+    end  
   end
 end
