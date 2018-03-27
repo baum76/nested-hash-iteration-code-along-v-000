110ae44bb077869a4815065b155fa017464ea7dc
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-27 20:53:52 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contact.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
     binding.pry
-  end  
+  end
 end
